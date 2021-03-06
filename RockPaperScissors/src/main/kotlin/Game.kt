f1739d3953a476597c483a9a8f02720a487fcc25
rafaelalma <72571530+rafael-alvarez-levelup@users.noreply.github.com>
2021-03-06 21:20:34 +0100

    Implement RockPaperScissors game

--- RockPaperScissors/src/main/kotlin/Game.kt
+++ RockPaperScissors/src/main/kotlin/Game.kt
@@ -1,6 +1,41 @@
 fun main() {
     val options = arrayOf("rock", "paper", "scissors")
     val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
+    printResult(userChoice, gameChoice)
 }
 
 fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
+
+fun getUserChoice(optionsParam: Array<String>): String {
+    var isValidChoice = false
+    var userChoice = ""
+
+    while (!isValidChoice) {
+        println("Please enter one of the following:")
+        for (item in optionsParam) print(" $item \n")
+
+        val userInput: String? = readLine()
+
+        if (userInput != null && userInput in optionsParam) {
+            isValidChoice = true
+            userChoice = userInput
+        } else {
+            println("You must enter a valid choice:")
+        }
+    }
+
+    return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String) {
+
+    val result: String = if (userChoice == gameChoice) "Tie!"
+    else if ((userChoice == "rock" && gameChoice == "scissors") ||
+        (userChoice == "paper" && gameChoice == "rock") ||
+        (userChoice == "scissors" && gameChoice == "paper")
+    ) "You win!"
+    else "I win!"
+
+    println("You chose $userChoice. I chose $gameChoice. $result")
+}
