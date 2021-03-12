3f610953439fe3a0c1101f5f45b81a0b4e991320
rafaelalma <rafaelalma92@gmail.com>
2021-03-12 17:51:30 +0100

    Improve Recipes project

--- Recipes/src/main/kotlin/Recipes.kt
+++ Recipes/src/main/kotlin/Recipes.kt
@@ -1,8 +1,37 @@
-data class Recipe(val title: String, val isVegetarian: Boolean)
+data class Recipe(
+    val title: String,
+    val mainIngredient: String,
+    val isVegetarian: Boolean = false,
+    val difficulty: String = "Easy"
+)
+
+class Mushroom(val size: Int, val isMagic: Boolean) {
+    constructor(isMagic_param: Boolean) : this(0, isMagic_param) {
+        // Code that runs when the secondary constructor is called
+    }
+}
+
+fun findRecipes(
+    title: String = "",
+    ingredient: String = "",
+    isVegetarian: Boolean = false,
+    difficulty: String = ""
+): Array<Recipe> {
+    // Code to find recipes
+    return arrayOf(Recipe(title, ingredient, isVegetarian, difficulty))
+}
+
+fun addNumbers(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun addNumbers(a: Double, b: Double): Double {
+    return a + b
+}
 
 fun main() {
-    val r1 = Recipe("Thai Curry", false)
-    val r2 = Recipe("Thai Curry", false)
+    val r1 = Recipe("Thai Curry", "Chicken")
+    val r2 = Recipe("Thai Curry", "Chicken")
     val r3 = r1.copy(title = "Chicken Bhuna")
 
     println("r1 hash code: ${r1.hashCode()}")
@@ -15,6 +44,15 @@ fun main() {
     println("r1 === r2? ${r1 === r2}")
     println("r1 == r3? ${r1 == r3}")
 
-    val (title, vegetarian) = r1
-    println("title is $title and vegetarian is $vegetarian")
+    val (title, mainIngredient, vegetarian, difficulty) = r1
+    println("title is $title, vegetarian is $vegetarian, mainIngredient is $mainIngredient and difficulty is $difficulty")
+
+    val m1 = Mushroom(6, false)
+    println("m1 size is ${m1.size} and isMagic is ${m1.isMagic}")
+
+    val m2 = Mushroom(true)
+    println("m2 size is ${m2.size} and isMagic is ${m2.isMagic}")
+
+    println(addNumbers(2, 5))
+    println(addNumbers(1.6, 6.3))
 }
