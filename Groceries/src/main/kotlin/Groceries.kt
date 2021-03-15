1c06993072d315fef3eabe32fb093d50c125d86b
rafaelalma <rafaelalma92@gmail.com>
2021-03-15 22:01:08 +0100

    Groceries project (Kotlin higher-order functions)

---
+++ Groceries/src/main/kotlin/Groceries.kt
@@ -0,0 +1,53 @@
+data class Grocery(
+    val name: String, val category: String,
+    val unit: String, val unitPrice: Double,
+    val quantity: Int
+)
+
+fun main() {
+    val groceries = listOf(
+        Grocery("Tomatoes", "Vegetable", "lb", 3.0, 3),
+        Grocery("Mushrooms", "Vegetable", "lb", 4.0, 1),
+        Grocery("Bagels", "Bakery", "Pack", 1.5, 2),
+        Grocery("Olive Oil", "Pantry", "Bottle", 6.0, 1),
+        Grocery("Ice Cream", "Frozen", "Pack", 3.0, 2)
+    )
+
+    val highestUnitPrice = groceries.maxByOrNull { it.unitPrice * 5 }
+    println("Highest Unit Price: $highestUnitPrice")
+
+    val lowestQuantity = groceries.minByOrNull { it.quantity }
+    println("Lowest Quantity: $lowestQuantity")
+
+    val sumQuantity = groceries.sumBy { it.quantity }
+    println("Sum Quantity: $sumQuantity")
+
+    val totalPrice = groceries.sumByDouble { it.quantity * it.unitPrice }
+    println("Total Price: $totalPrice")
+
+    val vegetables = groceries.filter { it.category == "Vegetable" }
+    println("Vegetables: $vegetables")
+
+    val notFrozen = groceries.filterNot { it.category == "Frozen" }
+    println("Not Frozen : $notFrozen")
+
+    val groceryNames = groceries.map { it.name }
+    println("Grocery Names: $groceryNames")
+
+    val halfUnitPrice = groceries.map { it.unitPrice * 0.5 }
+    println("Half Unit Price: $halfUnitPrice")
+
+    val newPrices = groceries.filter { it.unitPrice > 3.0 }.map { it.unitPrice * 2 }
+    println("New Prices: $newPrices")
+
+    println("Grocery names: ")
+    groceries.forEach { println(it.name) }
+
+    println("Groceries with unitPrice > 3.0: ")
+    groceries.filter { it.unitPrice > 3.0 }.forEach { println(it.name) }
+
+    // Closure
+    var itemNames = ""
+    groceries.forEach { itemNames += "${it.name} " }
+    println("Item names: $itemNames")
+}
