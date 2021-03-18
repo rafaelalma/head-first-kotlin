161cf7ca4583fb554b1c1a690762a9a9f85b1a87
rafaelalma <rafaelalma92@gmail.com>
2021-03-18 20:44:32 +0100

    Without coroutines

---
+++ DrumMachine/src/main/kotlin/Beats.kt
@@ -0,0 +1,30 @@
+import java.io.File
+import javax.sound.sampled.AudioSystem
+
+fun playBeats(beats: String, file: String) {
+    val parts = beats.split("x")
+    var count: Int
+    for (part in parts) {
+        count = part.length + 1
+        if (part == "") {
+            playSound(file)
+        } else {
+            Thread.sleep(100 * (part.length + 1L))
+            if (count < beats.length) {
+                playSound(file)
+            }
+        }
+    }
+}
+
+fun playSound(file: String) {
+    val clip = AudioSystem.getClip()
+    val audioInputStream = AudioSystem.getAudioInputStream(File(file))
+    clip.open(audioInputStream)
+    clip.start()
+}
+
+fun main() {
+    playBeats("x-x-x-x-x-x-", "toms.aiff")
+    playBeats("x-----x-----", "crash_cymbal.aiff")
+}
